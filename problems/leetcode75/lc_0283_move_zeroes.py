9885891e820eea44e5e30fe98e4d7675f4907b57
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-17 22:56:18 +0100

    feat(leetcode75): 0283

--- problems/leetcode75/lc_0283_move_zeroes.py
+++ problems/leetcode75/lc_0283_move_zeroes.py
@@ -1,3 +1,27 @@
 # Move Zeroes (https://leetcode.com/problems/move-zeroes/)
 # Difficulty: Easy
 # Tags: Array, Two Pointers
+
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """Do not return anything, modify nums in-place instead."""
+        i = 0
+        for n in nums:
+            if n != 0:
+                nums[i] = n
+                i += 1
+        for j in range(i, len(nums)):
+            nums[j] = 0
+        print(nums)
+ 
+solution = Solution()
+
+test_cases = [
+    ([0,1,0,3,12]), #[1,3,12,0,0]
+    ([0]),          #[0]
+]
+
+for nums in test_cases:
+    solution.moveZeroes(nums)
