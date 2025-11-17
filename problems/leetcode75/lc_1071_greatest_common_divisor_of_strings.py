d467d79813b99862508a6f1ab3fb5704f5058483
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-17 16:55:51 +0100

    feat(leetcode75): 1071

--- problems/leetcode75/lc_1071_greatest_common_divisor_of_strings.py
+++ problems/leetcode75/lc_1071_greatest_common_divisor_of_strings.py
@@ -1,3 +1,35 @@
 # Greatest Common Divisor of Strings (https://leetcode.com/problems/greatest-common-divisor-of-strings/)
 # Difficulty: Easy
 # Tags: Math, String
+
+from math import gcd
+
+class Solution1:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        max_len = gcd(len(str1), len(str2))
+        for i in range(max_len, 0, -1):
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                if str1[:i] * (len(str1) // i) == str1 and str1[:i] * (len(str2) // i) == str2:
+                    return str1[:i]
+        return ''
+                
+class Solution2:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ''
+        length_gcd = gcd(len(str1), len(str2))
+        return str1[:length_gcd]
+    
+solution1 = Solution1()
+solution2 = Solution2()
+
+test_cases = [
+    ("ABCABC", "ABC"),  #"ABC"
+    ("ABABAB", "ABAB"), #"AB"
+    ("LEET", "CODE")    #""
+]
+
+for str1, str2 in test_cases:
+    print(solution1.gcdOfStrings(str1, str2))
+for str1, str2 in test_cases:
+    print(solution2.gcdOfStrings(str1, str2))
