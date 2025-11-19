d4bdd199702d6eee1069fa3e3f9095cbb7bf1347
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-19 16:34:00 +0100

    feat(leetcode75): 1732

--- problems/leetcode75/lc_1732_find_the_highest_altitude.py
+++ problems/leetcode75/lc_1732_find_the_highest_altitude.py
@@ -1,3 +1,24 @@
 # Find the Highest Altitude (https://leetcode.com/problems/find-the-highest-altitude/)
 # Difficulty: Easy
 # Tags: Array, Prefix Sum
+
+from typing import List
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+            candidate = best = 0
+            for g in gain:
+                 candidate += g
+                 if candidate > best:
+                      best = candidate
+            return best
+
+solution = Solution()
+
+test_cases = [
+    ([-5,1,5,0,-7]), #1
+    ([-4,-3,-2,-1,4,3,2]), #0
+]
+
+for gain in test_cases:
+    print(solution.largestAltitude(gain))
