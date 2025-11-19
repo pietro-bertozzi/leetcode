459c840c4d162082f1b89b3214fff1f7b7450c9f
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-19 16:56:14 +0100

    feat(leetcode75): 0724

--- problems/leetcode75/lc_0724_find_pivot_index.py
+++ problems/leetcode75/lc_0724_find_pivot_index.py
@@ -1,3 +1,26 @@
 # Find Pivot Index (https://leetcode.com/problems/find-pivot-index/)
 # Difficulty: Easy
 # Tags: Array, Prefix Sum
+
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        summ = sum(nums)
+        left = 0
+        for i, n in enumerate(nums):
+            if left == summ - left - n:
+                return i
+            left += n
+        return -1
+
+
+solution = Solution()
+
+test_cases = [
+    ([1,7,3,6,5,6]), #3
+    ([1,2,3]), #-1
+]
+
+for s in test_cases:
+    print(solution.pivotIndex(s))
