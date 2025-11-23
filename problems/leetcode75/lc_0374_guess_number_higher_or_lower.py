c430e8bbe7714c0f7cfac8528a3a74fc9f21cf4f
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-23 19:42:57 +0100

    feat(leetcode75): 0374

--- problems/leetcode75/lc_0374_guess_number_higher_or_lower.py
+++ problems/leetcode75/lc_0374_guess_number_higher_or_lower.py
@@ -1,3 +1,47 @@
 # Guess Number Higher or Lower (https://leetcode.com/problems/guess-number-higher-or-lower/)
 # Difficulty: Easy
 # Tags: Binary Search, Interactive
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    picked = None
+
+    def guess(self, num: int) -> int:
+        global picked
+        if num > picked:
+            return -1
+        elif num < picked:
+            return 1
+        else:
+            return 0
+    
+    def guessNumber(self, n: int) -> int:
+        left = 1
+        right = n
+        while left <= right:
+            mid = (right+left)//2
+            response = self.guess(mid)
+            if response == -1:
+                right = mid - 1
+            elif response == 1:
+                left = mid + 1
+            else:
+                return mid
+
+solution = Solution()
+
+test_cases = [
+    (10, 6), #6
+    (1, 1),  #1
+    (2, 1)   #1
+]
+
+for n, num in test_cases:
+    picked = num
+    print(solution.guessNumber(n))
