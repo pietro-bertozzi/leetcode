2128ad7bb9eea94df708049c9ce13868dfc1a23e
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-23 21:30:14 +0100

    feat(leetcode75): 0746

--- problems/leetcode75/lc_0746_min_cost_climbing_stairs.py
+++ problems/leetcode75/lc_0746_min_cost_climbing_stairs.py
@@ -1,3 +1,22 @@
 # Min Cost Climbing Stairs (https://leetcode.com/problems/min-cost-climbing-stairs/)
 # Difficulty: Easy
 # Tags: Array, Dynamic Programming
+
+from typing import List
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return cost[-1]
+  
+solution = Solution()
+
+test_cases = [
+    ([10,15,20]),                 #15
+    ([1,100,1,1,1,100,1,1,100,1]) #6
+]
+
+for cost in test_cases:
+    print(solution.minCostClimbingStairs(cost))
