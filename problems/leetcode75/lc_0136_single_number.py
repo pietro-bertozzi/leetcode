215befe62869c9951f9b200391f542714b832bdf
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-25 13:03:21 +0100

    feat(leetcode75): 0136

--- problems/leetcode75/lc_0136_single_number.py
+++ problems/leetcode75/lc_0136_single_number.py
@@ -1,3 +1,23 @@
 # Single Number (https://leetcode.com/problems/single-number/)
 # Difficulty: Easy
 # Tags: Array, Bit Manipulation
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+        for n in nums:
+            single ^= n
+        return single
+        
+solution = Solution()
+
+test_cases = [
+    ([2,2,1]),     #1
+    ([4,1,2,1,2]), #4
+    ([1])          #1
+]
+
+for nums in test_cases:
+    print(solution.singleNumber(nums))
