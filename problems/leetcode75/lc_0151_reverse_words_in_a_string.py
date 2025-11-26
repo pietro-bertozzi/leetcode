63bd66ee0c55fd6e89d6a37c0a0859d390f8da00
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-26 13:03:11 +0100

    feat(leetcode75): 0151

--- problems/leetcode75/lc_0151_reverse_words_in_a_string.py
+++ problems/leetcode75/lc_0151_reverse_words_in_a_string.py
@@ -1,3 +1,36 @@
 # Reverse Words in a String (https://leetcode.com/problems/reverse-words-in-a-string/)
 # Difficulty: Medium
 # Tags: Two Pointers, String
+
+import re
+
+class Solution1:
+    def reverseWords(self, s: str) -> str:
+        s = re.sub(r"\s+", " ", s.strip()).split(" ")
+        s.reverse()
+        return " ".join(s)
+    
+class Solution2:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(re.sub(r"\s+", " ", s.strip()).split(" ")))
+    
+class Solution3:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(s.strip().split()[::-1])
+    
+solution1 = Solution1()
+solution2 = Solution2()
+solution3 = Solution3()
+
+test_cases = [
+    ("the sky is blue"), #"blue is sky the"
+    ("  hello world  "), #"world hello"
+    ("a good   example") #"example good a"
+]
+
+for s in test_cases:
+    print(solution1.reverseWords(s))
+for s in test_cases:
+    print(solution2.reverseWords(s))
+for s in test_cases:
+    print(solution3.reverseWords(s))
