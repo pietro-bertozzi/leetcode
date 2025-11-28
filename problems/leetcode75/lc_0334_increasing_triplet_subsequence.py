874947e0bd4a2556e46d5beac0f909781f8cf306
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2025-11-28 21:09:18 +0100

    feat(leetcode75): 0334

--- problems/leetcode75/lc_0334_increasing_triplet_subsequence.py
+++ problems/leetcode75/lc_0334_increasing_triplet_subsequence.py
@@ -1,3 +1,29 @@
 # Increasing Triplet Subsequence (https://leetcode.com/problems/increasing-triplet-subsequence/)
 # Difficulty: Medium
 # Tags: Array, Greedy
+
+import math
+from typing import List
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = math.inf #float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
+    
+solution = Solution()
+
+test_cases = [
+    ([1,2,3,4,5]),  #True
+    ([5,4,3,2,1]),  #False
+    ([2,1,5,0,4,6]) #True
+]
+
+for nums in test_cases:
+    print(solution.increasingTriplet(nums))
